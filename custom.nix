380188e9975fbdee205effdc50d40c0bd5bf0f94
John Rinehart <johnrichardrinehart@gmail.com>
2021-02-02 09:59:30 -0800

    feat: remove nginx server

--- custom.nix
+++ custom.nix
@@ -3,7 +3,7 @@
 	isoImage.makeEfiBootable=true;
 
 	networking.hostName = "mynixos";
-	services.nginx.enable=true;
+
 	environment.systemPackages = with pkgs; [ tmux vim ];
 	users.extraUsers.root.password = "somepassword";
 }
