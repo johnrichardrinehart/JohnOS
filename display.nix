f5a2227fdcce769a491d330b4030b21b2961a0e7
John Rinehart <johnrichardrinehart@gmail.com>
2021-02-02 09:47:27 -0800

    feat: add auto-login running dwm

--- display.nix
+++ display.nix
@@ -4,4 +4,9 @@
     services.xserver.enable = true;
     services.xserver.displayManager.startx.enable = true;
     services.xserver.windowManager.dwm.enable = true;
+    services.xserver.layout = "us";
+    services.xserver.variant = "dvorak";
+    services.xserver.displayManager.defaultSession = "none+dwm";
+    services.xserver.displayManager.autoLogin.enable = true;
+    services.xserver.displayManager.autoLogin.user = "root";
 }
