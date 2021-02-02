91bcb8419d06bc74a6fb55f26c9dfee3abdf690a
John Rinehart <johnrichardrinehart@gmail.com>
2021-02-02 09:50:15 -0800

    fix: bad attribute name for X kbd variant

--- display.nix
+++ display.nix
@@ -5,7 +5,7 @@
     services.xserver.displayManager.startx.enable = true;
     services.xserver.windowManager.dwm.enable = true;
     services.xserver.layout = "us";
-    services.xserver.variant = "dvorak";
+    services.xserver.xkbVariant = "dvorak";
     services.xserver.displayManager.defaultSession = "none+dwm";
     services.xserver.displayManager.autoLogin.enable = true;
     services.xserver.displayManager.autoLogin.user = "root";
