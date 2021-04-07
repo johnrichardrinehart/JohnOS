e1c4f9357681929ae0ed17ef1141c89f58364e97
John Rinehart <johnrichardrinehart@gmail.ocm>
2021-04-07 14:53:36 +0000

    feat: add stuff for iwlwifi module

--- networking.nix
+++ networking.nix
@@ -1,4 +1,11 @@
 {config, pkgs, ...}:
 {
-	networking.hostName = "mynixos";
+#	networking.networkmanager.enable = true;
+        config.networking.wireless.enable = true;
+        config.networking.wireless.networks = { AstroNet = { psk = "N0Pa$$w0rd"; }; };
+	config.networking.hostName = "mynixos";
+        config.boot.kernelPackages = pkgs.linuxPackages_latest;
+ 	config.hardware.enableRedistributableFirmware = true;
+        config.hardware.enableAllFirmware = true;
+        config.hardware.firmware = [ pkgs.wireless-regdb ];
 }
