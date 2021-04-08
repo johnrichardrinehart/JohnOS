f9591c19876128b4edf1396e7a7249a078ed95d7
John Rinehart <johnrichardrinehart@gmail.ocm>
2021-04-08 09:29:10 +0000

    refactor: remove unnecessary let..in block

--- default.nix
+++ default.nix
@@ -7,20 +7,15 @@ users ? {
     isNormalUser = true;
   };
 }, ...}:
-let
-   evalConfig = import <nixpkgs/nixos/lib/eval-config.nix>;
-   all = evalConfig {
-     system = builtins.currentSystem;
-     modules = [
-       <nixpkgs/nixos/modules/installer/cd-dvd/iso-image.nix>
-       ./iso.nix
-       ./ssh.nix
-       ./networking.nix
-       (import ./display.nix {autologinUser=(builtins.head (builtins.attrNames users));})
-       (import ./users.nix {users = users;})
-       ./packages.nix
-     ];
-   };
-in {
-  johnos = all.config.system.build.isoImage;
-}
+(import <nixpkgs/nixos/lib/eval-config.nix> {
+	system = builtins.currentSystem;
+	modules = [
+		<nixpkgs/nixos/modules/installer/cd-dvd/iso-image.nix>
+		./iso.nix
+		./ssh.nix
+		./networking.nix
+		(import ./display.nix {autologinUser=(builtins.head (builtins.attrNames users));})
+		(import ./users.nix {users = users;})
+		./packages.nix
+	];
+}).config.system.build.isoImage
