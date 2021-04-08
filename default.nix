ca6e853354aa9f83c0d5c8c4a794ee2af3e8a915
John Rinehart <johnrichardrinehart@gmail.ocm>
2021-04-08 08:48:14 +0000

    feat: add acpid events/actions for managing brightness

--- default.nix
+++ default.nix
@@ -8,18 +8,19 @@ users ? {
   };
 }, ...}:
 let
-   config = (import <nixpkgs/nixos/lib/eval-config.nix> {
-     system = "x86_64-linux";
+   evalConfig = import <nixpkgs/nixos/lib/eval-config.nix>;
+   all = evalConfig {
+     system = builtins.currentSystem;
      modules = [
        <nixpkgs/nixos/modules/installer/cd-dvd/iso-image.nix>
        ./iso.nix
        ./ssh.nix
        ./networking.nix
        (import ./display.nix {autologinUser=(builtins.head (builtins.attrNames users));})
-       (import ./users.nix {users = users;} )
+       (import ./users.nix {users = users;})
        ./packages.nix
      ];
-   }).config;
+   };
 in {
-  johnos = config.system.build.isoImage;
+  johnos = all.config.system.build.isoImage;
 }
