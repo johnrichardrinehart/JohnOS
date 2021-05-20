06acea018645e0a10e7a5c34496ce14029c019bc
John Rinehart <john.rinehart@ardanlabs.com>
2021-05-20 13:40:02 +0000

    refactor[configuration.nix]: major changes
    configuration.nix:
    1. has all commented options removed
    2. has the local timezone set (Europe/Kyiv)
    3. no longer manages the window manager (home-manager does)
    4. has the console kbd layout (TTY1, TTY2, etc.) match the X kbd layout (dvorak)
    5. has refactored a.b; a.c; --> a = {b = ...; c = ...;};
    6. has wget removed as a systemPackage

--- configuration.nix
+++ configuration.nix
@@ -9,23 +9,12 @@ args @ { config, pkgs, ... }:
     [
       # Include the results of the hardware scan.
       ./hardware-configuration.nix
-      ./wm
     ];
 
   # Use the GRUB 2 boot loader.
   boot.loader.grub.enable = true;
   boot.loader.grub.version = 2;
-  # boot.loader.grub.efiSupport = true;
-  # boot.loader.grub.efiInstallAsRemovable = true;
-  # boot.loader.efi.efiSysMountPoint = "/boot/efi";
-  # Define on which hard drive you want to install Grub.
   boot.loader.grub.device = "/dev/sda"; # or "nodev" for efi only
-
-  # networking.hostName = "nixos"; # Define your hostname.
-  # networking.wireless.enable = true;  # Enables wireless support via wpa_supplicant.
-
-  # Set your time zone.
-  # time.timeZone = "Europe/Amsterdam";
 
   # The global useDHCP flag is deprecated, therefore explicitly set to false here.
   # Per-interface useDHCP will be mandatory in the future, so this generated config
@@ -33,75 +22,39 @@ args @ { config, pkgs, ... }:
   networking.useDHCP = false;
   networking.interfaces.enp0s3.useDHCP = true;
 
-  # Configure network proxy if necessary
-  # networking.proxy.default = "http://user:password@proxy:port/";
-  # networking.proxy.noProxy = "127.0.0.1,localhost,internal.domain";
-
-  # Select internationalisation properties.
-  # i18n.defaultLocale = "en_US.UTF-8";
-  # console = {
-  #   font = "Lat2-Terminus16";
-  #   keyMap = "us";
-  # };
-
-  #  # Enable the X11 windowing system.
-  #  services.xserver.enable = true;
-  #  services.xserver.displayManager.defaultSession = "gnome";
-  #
-  #  # Enable the GNOME 3 Desktop Environment.
-  #  services.xserver.displayManager.gdm.enable = true;
-  #  services.xserver.desktopManager.gnome.enable = true;
-
-  # Enable the X11 windowing system.
-  services.xserver.enable = true;
-  #  services.xserver.displayManager.lightdm.enable = true;
-  #services.xserver.displayManager.lightdm.enable = false;
-  services.xserver.displayManager.lightdm.greeters.tiny.enable = true;
-  services.xserver.displayManager.defaultSession = "home-manager";
-  # Configure keymap in X11
-  services.xserver.layout = "us";
-  services.xserver.xkbVariant = "dvorak";
-  # services.xserver.xkbOptions = "eurosign:e";
-
-  # Enable CUPS to print documents.
-  # services.printing.enable = true;
-
-  # Enable sound.
-  # sound.enable = true;
-  # hardware.pulseaudio.enable = true;
-
-  # Enable touchpad support (enabled default in most desktopManager).
-  # services.xserver.libinput.enable = true;
-
-
-  # Some programs need SUID wrappers, can be configured further or are
-  # started in user sessions.
-  # programs.mtr.enable = true;
-  # programs.gnupg.agent = {
-  #   enable = true;
-  #   enableSSHSupport = true;
-  # };
-
-  # List services that you want to enable:
-
-  # Enable the OpenSSH daemon.
-  services.openssh.enable = true;
-
-  # Open ports in the firewall.
-  # networking.firewall.allowedTCPPorts = [ ... ];
-  # networking.firewall.allowedUDPPorts = [ ... ];
-  # Or disable the firewall altogether.
-  # networking.firewall.enable = false;
-
-  # This value determines the NixOS release from which the default
-  # settings for stateful data, like file locations and database versions
-  # on your system were taken. It‘s perfectly fine and recommended to leave
-  # this value at the release version of the first install of this system.
-  # Before changing this value read the documentation for this option
-  # (e.g. man configuration.nix or on https://nixos.org/nixos/options.html).
   system.stateVersion = "21.05"; # Did you read the comment?
 
   ##### John's Stuff #####
+
+  # Set your time zone.
+  time.timeZone = "Europe/Kyiv";
+
+  console.useXkbConfig = true;
+
+  # Configure keymap in X11
+  services.xserver = {
+    enable = true;
+    layout = "us";
+    xkbVariant = "dvorak";
+    displayManager = {
+      # https://www.reddit.com/r/unixporn/comments/a7rg63/oc_a_tiny_riceable_lightdm_greeter/eckzt15?utm_source=share&utm_medium=web2x&context=3
+      defaultSession = "default"; # TODO: figure out a way to use another string besides default
+      lightdm.greeters.tiny.enable = true;
+      # https://discourse.nixos.org/t/opening-i3-from-home-manager-automatically/4849/8
+    };
+    desktopManager.session = [
+      {
+        manage = "window";
+        name = "default";
+        start = ''
+                  ${pkgs.runtimeShell} $HOME/.hm-xsession &
+                  waitPID=$!
+          #        exec $HOME/.hm-xsession
+        '';
+      }
+    ];
+  };
+
   fonts.fonts = with pkgs; [ fira-code fira-code-symbols ];
 
   programs.zsh.enable = true;
@@ -122,10 +75,8 @@ args @ { config, pkgs, ... }:
 
 
   # List packages installed in system profile. To search, run:
-  # $ nix search wget
   environment.systemPackages = with pkgs; [
     args.agenix.defaultPackage.x86_64-linux
-    wget
   ];
 
   age.sshKeyPaths = [ "/home/john/.ssh/id_rsa" "/etc/ssh/ssh_host_rsa_key" "/etc/ssh/ssh_host_ed25519_key" ];
@@ -148,16 +99,4 @@ args @ { config, pkgs, ... }:
     nixPath = [ "nixpkgs=${args.nixpkgs}" ];
   };
 
-  console.useXkbConfig = true;
-
-  # https://discourse.nixos.org/t/opening-i3-from-home-manager-automatically/4849/8
-  services.xserver.desktopManager.session = [
-    {
-      name = "home-manager";
-      start = ''
-        ${pkgs.runtimeShell} $HOME/.hm-xsession &
-        waitPID=$!
-      '';
-    }
-  ];
 }
