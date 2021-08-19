0f9391334b3670cd8d50bddd20295b58d5ea4703
John Rinehart <john.rinehart@ardanlabs.com>
2021-08-20 01:35:03 +0300

    feat(users/polybar/modules.ini): use the proper battery settings
    fix(wm/xmonad.nix): fix keyboard and audio after start-up
    chore(users/polybar/config.ini): reduce the font size of the bar

--- wm/xmonad.nix
+++ wm/xmonad.nix
@@ -21,6 +21,16 @@ let
     #  setxkbmap -model sun_type7_usb -layout gb -option ctrl:swapcaps
     #  setxkbmap -model pc104 -layout cz,us -variant ,dvorak -option grp:alt_shift_toggle
     #  localectl [--no-convert] set-x11-keymap layout [model [variant [options]]]
+
+    ## Set laptop keyboard to dvorak after xmonad screws up my Xorg.conf
+    ${pkgs.xorg.setxkbmap}/bin/setxkbmap -device $(${pkgs.xorg.xinput}/bin/xinput | grep "AT Translated Set 2 keyboard" | cut -f 2 | cut -d = -f 2) -layout dvorak
+
+    ## Fix linux kernel's ability to process audio 
+    ## c.f. https://askubuntu.com/questions/1263178/20-04-no-speaker-audio-on-hp-spectre-x360-2020-15t-eb000
+    ${pkgs.alsa-tools}/bin/hda-verb /dev/snd/hwC0D0 0x01 SET_GPIO_DIR 0x01
+    ${pkgs.alsa-tools}/bin/hda-verb /dev/snd/hwC0D0 0x01 SET_GPIO_MASK 0x01
+    ${pkgs.alsa-tools}/bin/hda-verb /dev/snd/hwC0D0 0x01 SET_GPIO_DATA 0x01
+    ${pkgs.alsa-tools}/bin/hda-verb /dev/snd/hwC0D0 0x01 SET_GPIO_DATA 0x00
   '';
 
   polybarOpts = ''
