43af38b8f679b160408c078fd080efd55b8aed1a
John Rinehart <john.rinehart@ardanlabs.com>
2021-08-19 10:18:55 +0300

    feat(laptop.nix): set default resolution and add WiFi network support (still need to manually start wpa_supplicant service)

--- laptop.nix
+++ laptop.nix
@@ -1,12 +1,31 @@
 args @ { config, pkgs, ... }:
 {
-
-  services.xserver.dpi = 4;
-  networking.wireless.userControlled.enable = true;
-  networking.wireless.networks.EpsteinDidntKillHimself5G.pskRaw = "1ccdb453d26a04c707084d33728e26a34c78f8712f878366b4ad129800dff828";
-  networking.networkmanager.enable = true;
   hardware.pulseaudio.enable = true;
 
   # disabled by installation-cd-minimal
   fonts.fontconfig.enable = pkgs.lib.mkForce true;
+
+  services.xserver.resolutions = [
+    {
+      "x" = 1920;
+      "y" = 1080;
+    }
+  ];
+
+  networking = {
+    hostName = "johnos"; # Put your hostname here.
+    useDHCP = false;
+    interfaces.wlo1.useDHCP = true;
+    wireless = {
+      interfaces = [
+        "wlo1"
+      ];
+      enable = true;
+      networks = {
+        EpsteinDidntKillHimself5G = {
+          pskRaw = "1ccdb453d26a04c707084d33728e26a34c78f8712f878366b4ad129800dff828";
+        };
+      };
+    };
+  };
 }
