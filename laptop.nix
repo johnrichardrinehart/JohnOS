b045313a12846bdf4eefe5aa5cea0183a187e7dc
John Rinehart <johnrichardrinehart@gmail.com>
2021-08-26 10:00:35 +0300

    feat(nvidia): add support for multiple monitors

--- laptop.nix
+++ laptop.nix
@@ -94,25 +94,9 @@ args @ { config, pkgs, ... }:
   ########## NVIDIA offload
   ################################################################################
   #  services.xserver.videoDrivers = [ "modesetting" "nouveau" "nvidia" ];
-  services.xserver.videoDrivers = [ "modesetting" "nvidia" ];
-  hardware.nvidia.prime = {
-    offload.enable = true;
-
-    # Bus ID of the Intel GPU. You can find it using lspci, either under 3D or VGA
-    intelBusId = "PCI:0:2:0";
-
-    # Bus ID of the NVIDIA GPU. You can find it using lspci, either under 3D or VGA
-    nvidiaBusId = "PCI:1:0:0";
-  };
-  hardware.nvidia.package = config.boot.kernelPackages.nvidiaPackages.vulkan_beta;
-  hardware.nvidia.nvidiaPersistenced = true;
-
-  ################################################################################
-  ########## NVIDIA sync
-  ################################################################################
-  #  services.xserver.videoDrivers = [ "modesetting" "nouveau" "nvidia" ];
+  #  services.xserver.videoDrivers = [ "modesetting" "nvidia" ];
   #  hardware.nvidia.prime = {
-  #    sync.enable = true;
+  #    offload.enable = true;
   #
   #    # Bus ID of the Intel GPU. You can find it using lspci, either under 3D or VGA
   #    intelBusId = "PCI:0:2:0";
@@ -120,6 +104,28 @@ args @ { config, pkgs, ... }:
   #    # Bus ID of the NVIDIA GPU. You can find it using lspci, either under 3D or VGA
   #    nvidiaBusId = "PCI:1:0:0";
   #  };
+  hardware.nvidia.package = config.boot.kernelPackages.nvidiaPackages.vulkan_beta;
+  hardware.nvidia.nvidiaPersistenced = true;
+
+  ################################################################################
+  ########## NVIDIA sync
+  ################################################################################
+  services.xserver.videoDrivers = [ "nvidia" ];
+  hardware.nvidia =
+    {
+      modesetting.enable = true;
+
+      prime = {
+        sync.enable = true;
+
+
+        # Bus ID of the Intel GPU. You can find it using lspci, either under 3D or VGA
+        intelBusId = "PCI:0:2:0";
+
+        # Bus ID of the NVIDIA GPU. You can find it using lspci, either under 3D or VGA
+        nvidiaBusId = "PCI:1:0:0";
+      };
+    };
 
   ################################################################################
   ########## bumblebee
