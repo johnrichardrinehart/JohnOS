59447ebda598eae93a43852553a07430e007469c
John Rinehart <john.rinehart@ardanlabs.com>
2022-01-10 10:57:45 +0200

    fix: various tweaks and fixes to build_and_split.sh

--- .github/build_and_split.sh
+++ .github/build_and_split.sh
@@ -14,7 +14,8 @@ SPLIT_DIR=${1:-${GITHUB_WORKSPACE}/split}
 # 12 characters should be more than enough - Linus recommends this for the
 # developers of the Linux kernel
 SHORT_SHA=${GITHUB_SHA:0:12}
-CHECKSUM_FILE="${SPLIT_DIR}/checksums-${SHORT_SHA}.sha256"
+CHECKSUM_FILE="${SPLIT_DIR}/checksums-${GITHUB_REF_NAME}-${SHORT_SHA}.sha256"
+SPLIT_BASENAME_PREFIX="JohnOS-${GITHUB_REF_NAME}-${SHORT_SHA}.iso."
 
 mkdir -p "${SPLIT_DIR}"
 
@@ -32,7 +33,7 @@ echo "splitting the ISO into pieces"
 cd "${NIX_BUILD_RESULT_ISO_DIR}"
 NIX_BUILD_RESULT_ISO_NAME="" # default empty
 for iso in $(ls -1 *.iso); do
-  if [[ NIX_BUILD_RESULT_ISO_NAME != "" ]]; then
+  if [[ "${NIX_BUILD_RESULT_ISO_NAME}" != "" ]]; then
     tree -l L 8 "${GITHUB_WORKSPACE}"
     echo "we have more than one built ISO. we shouldn't..."
     exit 1
@@ -43,7 +44,7 @@ done
 NIX_BUILD_RESULT_ISO="${NIX_BUILD_RESULT_ISO_DIR}/${NIX_BUILD_RESULT_ISO_NAME}"
 split -d -b 128MiB \
   "${NIX_BUILD_RESULT_ISO}" \
-  "${SPLIT_DIR}/JohnOS-*.iso."
+  "${SPLIT_DIR}/"${SPLIT_BASENAME_PREFIX}"
 
 echo "generating checksum file"
 # generate SHA256 checksums of all pieces
