a75aadb961a7b6e49f85f00d23e346b643536927
John Rinehart <johnrichardrinehart@gmail.com>
2022-03-02 01:40:28 +0200

    fix: pin to a recent (and stable) nix version

--- flake.nix
+++ flake.nix
@@ -17,6 +17,7 @@
     flake-templates.url = "github:NixOS/templates";
 
     nix-master.url = "github:NixOS/nix/master";
+    nix-2_6_1.url = "github:nixos/nix/e044ccb67ce38d11059de32515306f5f1bd2f04f";
 
     home-manager = {
       url = "github:nix-community/home-manager/release-21.11";
@@ -27,7 +28,7 @@
 
   outputs = inputs:
     let
-      nix_pkg = inputs.nix-master.packages.x86_64-linux.nix;
+      nix_pkg = inputs.nix-2_6_1.packages.x86_64-linux.nix;
 
       nixpkgs-unstable = inputs.nixpkgs_unstable;
 
