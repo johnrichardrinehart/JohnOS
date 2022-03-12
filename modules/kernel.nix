a77dcfed4ac6ebdaeaaec7c0703527d023c0cbd8
John Rinehart <johnrichardrinehart@gmail.com>
2022-03-12 15:07:38 +0200

    refactor: simplify the kernel configuration

--- modules/kernel.nix
+++ modules/kernel.nix
@@ -3,30 +3,23 @@
   nixpkgs.config.allowBroken = true; # zfs-kernel-$VERSION is broken
 
   # add JohnOS-kernel as a package
-  nixpkgs = {
-    overlays = [
-      (self: super:
-        let
-          latest_stable_pkg = { fetchurl, buildLinux, ... } @ args:
-            buildLinux (args // rec {
-              version = "5.16.14";
-              modDirVersion = "5.16.14";
-
-              kernelPatches = [ ];
-
-              src = fetchurl {
-                url = "https://cdn.kernel.org/pub/linux/kernel/v5.x/linux-${version}.tar.xz";
-                sha256 = "sha256-u1od8VoQpxWAekSHL/T+d1M3quRFKFGB8dG6DHix1/I=";
-              };
-
-            } // (args.argsOverride or { }));
-          latest_stable = pkgs.callPackage latest_stable_pkg { };
-        in
-        {
-          JohnOS-kernel = pkgs.linuxPackagesFor latest_stable;
-        })
-    ];
-  };
+  nixpkgs.overlays = [
+    (
+      self: super: {
+        JohnOS-kernel = super.linuxPackagesFor (super.linux_latest.override {
+          argsOverride = rec {
+            src = super.fetchurl {
+              url = "mirror://kernel/linux/kernel/v5.x/linux-${version}.tar.xz";
+              sha256 = "sha256-eoulhlnV5fD54eCk++05rFIBSdJNeuxGNvz4JV0FdPY=";
+            };
+            version = "5.16.14";
+            modDirVersion = "5.16.14";
+          };
+          ignoreConfigErrors = true;
+        });
+      }
+    )
+  ];
 
   boot.kernelPackages = pkgs.JohnOS-kernel;
 }
