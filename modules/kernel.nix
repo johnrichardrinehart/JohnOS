843cd0559a1df3cb1d2b8bd4d3089c036f0900df
John Rinehart <johnrichardrinehart@gmail.com>
2022-05-25 12:14:52 +0100

    feat(modules/kernel.nix): bump the kernel version

--- modules/kernel.nix
+++ modules/kernel.nix
@@ -8,14 +8,14 @@
       self: super: {
         JohnOS-kernel =
           let
-            v = "5.17.9";
+            v = "5.18";
             os_name = "-JohnOS";
           in
           super.linuxPackagesFor (super.linux_latest.override {
             argsOverride = rec {
               src = super.fetchurl {
                 url = "mirror://kernel/linux/kernel/v5.x/linux-${version}.tar.xz";
-                sha256 = "sha256-FyQkvEHvLfmxlFfOsCK1alHrlJdSmxXOfpuNb5CtWXg=";
+                sha256 = "sha256-UfPxaEqJbnlxgqCQcpnMHw/15bUd2aVUeK5jpAmFXO4=";
               };
 
               version = "${v}";
