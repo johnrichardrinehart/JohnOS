df12a960bdf6dfd09b1e17a304d130ad98d78965
John Rinehart <johnrichardrinehart@gmail.com>
2022-09-04 13:30:57 +0100

    feat: add vscodium overlay

--- modules/overlays.nix
+++ modules/overlays.nix
@@ -7,13 +7,16 @@
       versions = {
         "1.69.2" = "sha256-xpNqBVB1iD/NQtaW7nHZ5GBvC8nJxA27FOhfnr2ydU8=";
         "1.70.0" = "sha256-AclhcuFd0ajAiALpJyYRGQ7PA2IS2luOVtMZot3plm8=";
+        "local" = "";
       };
     version = "1.69.2";
     sha256 = versions.${version};
     in
       {
-      src = super.fetchurl {
-        url = "https://github.com/VSCodium/vscodium/releases/download/${version}/VSCodium-${plat}-${version}.${archive_fmt}";
+        src = if version == "local" then
+        "/home/john/code/repos/others/microsoft/vscode"
+        else super.fetchurl {
+        url =  "https://github.com/VSCodium/vscodium/releases/download/${version}/VSCodium-${plat}-${version}.${archive_fmt}";
         inherit sha256;
       }; 
       inherit version;
