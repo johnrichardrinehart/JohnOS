57cbd947120c8db1c943dfef5943b065bce8e681
John Rinehart <johnrichardrinehart@gmail.com>
2022-12-22 12:50:44 +0000

    feat: linux 6.0.5 → 6.1.1

--- modules/kernel.nix
+++ modules/kernel.nix
@@ -8,14 +8,14 @@
       self: super: {
         myLinux =
           let
-            v = "6.0.5"; # before a bunch of ALSA usb-audio changes
+            v = "6.1.1"; # before a bunch of ALSA usb-audio changes
             os_name = "-JohnOS";
           in
           super.linuxPackagesFor (super.linux_latest.override {
             argsOverride = rec {
               src = super.fetchurl {
                 url = "https://cdn.kernel.org/pub/linux/kernel/v6.x/linux-${v}.tar.xz";
-                sha256 = "sha256-YTMu8itTxQwQ+qv7lliWp9GtTzOB8PiWQ8gg8opgQY4=";
+                sha256 = "sha256-o+YTd89ENanilmtAmjehBW9qqlnlYa3ZElqI48CXHfs=";
               };
 
               version = v;
