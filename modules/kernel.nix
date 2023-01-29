b6c83c785d024c887c5a60266cfbd1cfe6f76546
John Rinehart <johnrichardrinehart@gmail.com>
2023-01-29 00:11:33 +0000

    feat: linux 6.1.7 → 6.1.8
    chore: bump kernel inputs

--- modules/kernel.nix
+++ modules/kernel.nix
@@ -8,14 +8,14 @@
       self: super: {
         myLinux =
           let
-            v = "6.1.7"; # before a bunch of ALSA usb-audio changes
+            v = "6.1.8"; # before a bunch of ALSA usb-audio changes
             os_name = "-JohnOS";
           in
           super.linuxPackagesFor (super.linux_latest.override {
             argsOverride = rec {
               src = super.fetchurl {
                 url = "https://cdn.kernel.org/pub/linux/kernel/v6.x/linux-${v}.tar.xz";
-                sha256 = "sha256-SrBIutLnOA07gn8frVrS0vxPLoDhxgTYXR+Hgd6+YA8=";
+                sha256 = "sha256-tgu1Ori6NwonBFSxHpPUGvKRJvxyvW7eUXZz4uV7gW0=";
               };
 
               version = v;
