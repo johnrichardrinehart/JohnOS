bcc6826f33be497176599849d8b81a7f68f7d453
John Rinehart <johnrichardrinehart@gmail.com>
2023-02-06 15:23:36 +0000

    feat: remove vscodium overlay

--- modules/overlays.nix
+++ modules/overlays.nix
@@ -1,31 +1,5 @@
 [
   (self: super: {
-    vscodium = super.vscodium.overrideAttrs (final: prev:
-      (
-        let
-          plat = "linux-x64";
-          archive_fmt = "tar.gz";
-          versions = {
-            "1.69.2" = "sha256-xpNqBVB1iD/NQtaW7nHZ5GBvC8nJxA27FOhfnr2ydU8=";
-            "1.70.0" = "sha256-AclhcuFd0ajAiALpJyYRGQ7PA2IS2luOVtMZot3plm8=";
-            "local" = "";
-          };
-          version = "1.69.2";
-          sha256 = versions.${version};
-        in
-        {
-          src =
-            if version == "local" then
-              "/home/john/code/repos/others/microsoft/vscode"
-            else
-              super.fetchurl {
-                url = "https://github.com/VSCodium/vscodium/releases/download/${version}/VSCodium-${plat}-${version}.${archive_fmt}";
-                inherit sha256;
-              };
-          inherit version;
-        }
-      ));
-
     python310Packages = super.python310Packages // ({
       poetry = super.python310Packages.poetry.overrideAttrs (final: prev:
         {
