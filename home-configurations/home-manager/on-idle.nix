212fbf87dff37aabfb5f25942fc84cef0f92980d
John Rinehart <johnrichardrinehart@gmail.com>
2025-12-15 09:38:30 -0500

    fix: no fail if no VT sessions
    chore: give a couple more seconds between notification and lock

--- home-configurations/home-manager/on-idle.nix
+++ home-configurations/home-manager/on-idle.nix
@@ -5,9 +5,9 @@
 , jq
 }:
 
-writeShellScriptBin "lock-vt-sessions" ''
+writeShellScriptBin "on-idle" ''
   ${libnotify}/bin/notify-send -u normal 'Idle... locking (and killing all VT sessions)' && \
-  sleep 3 && \
-  ${systemd}/bin/loginctl kill-session $(${systemd}/bin/loginctl list-sessions --json=short | ${jq}/bin/jq -r 'map(select(.tty != "tty1" and .tty != null) | .session) | join(" ")') && \
-  ${systemd}/bin/loginctl lock-session
+  sleep 5 && \
+  ${systemd}/bin/loginctl lock-session && \
+  ${systemd}/bin/loginctl kill-session $(${systemd}/bin/loginctl list-sessions --json=short | ${jq}/bin/jq -r 'map(select(.tty != "tty1" and .tty != null) | .session) | join(" ")')
 ''
